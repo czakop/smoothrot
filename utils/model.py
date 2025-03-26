3a2e6dce662dd81253f305ac3e9610e3044727e0
Patrik Czako <czako.patrik98@gmail.com>
2025-03-26 19:43:36 +0100

    feat: add support for Mistral and Llama3 models

--- utils/model.py
+++ utils/model.py
@@ -15,7 +15,10 @@ if TYPE_CHECKING:
 class ModelType(str, Enum):
     LLAMA_2_7B = "meta-llama/Llama-2-7b-hf"
     LLAMA_2_13B = "meta-llama/Llama-2-13b-hf"
+    LLAMA_3_1_8B = "meta-llama/Llama-3.1-8B"
     LLAMA_3_2_1B = "meta-llama/Llama-3.2-1B"
+    LLAMA_3_2_3B = "meta-llama/Llama-3.2-3B"
+    MISTRAL_7B = "mistralai/Mistral-7B-v0.3"
 
 
 def load_model(
